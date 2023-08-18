9a4a02edc808ce1ff1d1d57a4ebd7595b68d0075
Roman Organ <r.organ@dan-it.com.ua>
2023-08-19 01:17:51 +0300

    update

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,147 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+import { threadId } from "worker_threads";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export const createThread = async ({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) => {
+  try {
+    connectToDB();
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
+
+    // Update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
+  }
+};
+
+export const fetchThreads = async (pageNumber = 1, pageSize = 20) => {
+  connectToDB();
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //fetch the threads that have no parents (top-level threads...)
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalThreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+  const threads = await threadsQuery.exec();
+
+  const isNext = totalThreadsCount > skipAmount + threads.length;
+
+  return { threads, isNext };
+};
+
+export const fetchThreadById = async (id: string) => {
+  connectToDB();
+
+  //TODO: Populate Community
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
+};
+
+export const addCommentToThread = async (
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) => {
+  connectToDB();
+
+  try {
+    // find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    // Save the thread
+    const savedCommentThread = await commentThread.save();
+
+    // Update the original thread to include the new comment
+    originalThread.children.push(savedCommentThread._id);
+
+    //Save the original thread
+    await originalThread.save()
+
+  } catch (error: any) {
+    throw new Error(`Error adding comment: ${error.message}`);
+  }
+};
